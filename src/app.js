4b6608f39ec47143be48a7da1f9506a4dd63a9e4
AlbertoWagner <alberto.oliveira@ccc.ufcg.edu.br>
2018-09-06 02:55:12 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express =  require('express');
 const bodyParser =  require('body-parser');
-
+const mongoose = require('mongodb')
 const  app = express();
-const router = express.Router();
+const router =express.Router();
+
+// connectar ao banco
+
+var mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://localhost/workshoptdc")
+    .then(conn => global.conn = conn.db("workshoptdc"))
+    .catch(err => console.log(err))
+
+module.exports = { findAll}
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extend:false}));
+
+function findAll(callback){
+    global.conn.collection("customers").find({}).toArray(callback);
+}
 
 const  route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -18,6 +31,9 @@ const  route = router.get('/', (req, res, next) => {
 const  create = router.post('/',(req,res,next) =>{
     res.status(201).send(req.body);
 });
+
+
+/* GET home page. */
 
 app.use("/", route);
 app.use('/products' , create);
