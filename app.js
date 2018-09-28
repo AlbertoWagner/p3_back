758f2ebf65fee3c4f5e2155b3249b816469afe27
AlbertoWagner <alberto.oliveira@ccc.ufcg.edu.br>
2018-09-27 23:07:58 -0300

    Novo projeto portal de noticias

--- app.js
+++ app.js
@@ -1,59 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/',function (req,res) {
+    res.render("home/index");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/fomulario_inclusao_noticia',function (req,res) {
+    res.render("admin/form_add_noticia");
 });
 
-const router = express.Router();
-const bodyParser = require("body-parser");
-
-//Rotas
-const index = require('./routes/index');
-const personRoute = require('./routes/personRoute');
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(bodyParser.json());
+app.get('/noticia',function (req,res) {
+    res.render("noticias/noticias");
+});
 
 
-app.use('/', index);
-app.use('/person', personRoute);
 
-
-module.exports = app;
+app.listen(3000,function () {
+    console.log('Servidor')
+});
--- config/db.js
+++
@@ -1,5 +0,0 @@
-// config/db.js
-module.exports = {
-    url : "",
-    local_url: "mongodb://localhost:27017/p3_back"
-}
