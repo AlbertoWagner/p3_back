3585cdbf649a32d879aedbeb0c577329fda6eb83
AlbertoWagner <alberto.oliveira@ccc.ufcg.edu.br>
2018-09-28 23:31:42 -0300

    banco mysql

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
+var rotaNoticias =  require('./app/routes/noticias');
+rotaNoticias(app);
+var rotaHome = require('./app/routes/home');
+rotaHome(app);
+var rotaFormmularioInclusaoNoticia = require('./app/routes/formmulario_inclusao_noticia');
+rotaFormmularioInclusaoNoticia(app);
 
-
-app.set('view engine', 'ejs');
-
-app.get('/',function (req,res) {
-    res.render("home/index");
-});
-
-app.get('/fomulario_inclusao_noticia',function (req,res) {
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticia',function (req,res) {
-    res.render("noticias/noticias");
-});
 
 
 
 app.listen(3000,function () {
-    console.log('Servidor')
+    console.log("ON");
+
 });
